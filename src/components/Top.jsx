951ce1c2df8014b348a0b9c2cdd5dc36fe14c791
Daniel Santos <daniel.santos@walmart.com>
2020-07-08 17:29:40 -0400

    reset project w/ min boilerplate

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,35 +1,12 @@
 import React from "react";
 
-const Top = props => {
-  const {
-    appOnUpdateName,
-    appCurrentUser,
-    appValidation,
-    appOnButtonClick
-  } = props;
-  //Lifting up state
-  const handleChange = event => appOnUpdateName(event.target.value);
+const Top = (props) => {
+  const { show } = props;
 
-  /*JSX RENDERER*/
-  return (
+  return show ? (
     <header style={{ display: "flex", flexDirection: "column" }}>
-      <form style={{ display: "flex" }}>
-        <input
-          type="text"
-          placeholder={appCurrentUser ? "" : "Write something here"}
-          value={appCurrentUser}
-          onChange={handleChange}
-        />
-        <button onClick={appOnButtonClick}>Add</button>
-      </form>
-      <span
-        className={`validation ${
-          appValidation.length > 0 ? "notHidden" : "hidden"
-        }`}
-      >
-        {appValidation}
-      </span>
+      I'm the header
     </header>
-  );
+  ) : null;
 };
 export default Top;
