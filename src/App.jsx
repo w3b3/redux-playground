8931f9dfd192999aaff006a7c77d8ba65e023394
Daniel Santos <daniel.santos@walmart.com>
2020-07-09 00:05:45 -0400

    simplify removing Top component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import Top from "./components/Top";
+// import Top from "./components/Top";
 import Bottom from "./components/Bottom";
 import Container from "@material-ui/core/Container";
 import { callApi } from "./api";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
   const [apiResponse, setApiResponse] = useState({});
 
   useEffect(() => {
     callApi().then((r) => {
-      // console.table(r);
       setApiResponse(r);
-      setLoading(false);
+      // setLoading(false);
     });
   }, []);
 
   return (
     <Container className="App">
-      <Top show={!loading} />
+      {/*<Top show={!loading} />*/}
       <Bottom data={apiResponse} />
     </Container>
   );
